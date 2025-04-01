fc2d1f5e5c7f15a5cc7a508aa3fd71d9499352c0
philosophy1106 <ahhyun1106@gmail.com>
2025-04-01 11:10:27 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3490dc",
+        secondary: "#ffed4a",
+        danger: "#e3342f",
+        clifford: "#da373d",
+        carrot: "#e67e22",
+        main: "#54a0ff",
+      },
+    },
+  },
+};
