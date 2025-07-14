4c7c7b7ed4befc075694af407e0ce25a0c671288
philosophy1106 <ahhyun1106@gmail.com>
2025-07-14 16:55:05 +0900

    10장 람다 표현식: stream 활용(추가)

--- java/basic/src/oop_ex10/LambdaTest.java
+++ java/basic/src/oop_ex10/LambdaTest.java
@@ -2,6 +2,8 @@ package oop_ex10;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class LambdaTest {
 	public static void main(String[] args) {
@@ -9,7 +11,7 @@ public class LambdaTest {
 		System.out.println("짝수의 개수");
 		System.out.println(list.stream().filter(i -> i % 2 == 0).count());
 		System.out.println("각 숫자를 제곱");
-		System.out.println(list.stream().map(e -> e * e).toList());
+		System.out.println(list.stream().map(i -> i * i).toList());
 		System.out.println("중복 제거");
 		System.out.println(list.stream().distinct().toList());
 		System.out.println("기본 정렬");
@@ -22,9 +24,14 @@ public class LambdaTest {
 		System.out.println(list.stream().skip(5).toList());
 		System.out.println("값이 5보다 큰 것만 출력");
 		System.out.println(list.stream().filter(i -> i > 5).toList());
-		System.out.println("1~10의 합계"); //뭔 얘기지? skip?
+		System.out.println("전체 합계 / 전체 평균");
 		System.out.println(list.stream().reduce(Integer::sum).orElse(0));
-		System.out.println("1 ~ 10의 평균");
 		System.out.println(list.stream().mapToInt(Integer::intValue).average().orElse(0));
+		System.out.println("1~10의 합계"); //숫자 1부터 10까지 합
+		System.out.println(IntStream.rangeClosed(1, 10));
+		System.out.println("랜덤 5개의 평균");
+		System.out.println(
+			"Stream.generate(Math::random).limit(5).mapToDouble(Double::doubleValue).average() = " + Stream.generate(
+				Math::random).limit(5).mapToDouble(Double::doubleValue).average());
 	}
 }
